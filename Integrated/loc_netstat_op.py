1894f6b841e20f87529d174b909c76ea5241466b
ombahiwal <ombahiwal@gmail.com>
2019-02-25 20:54:28 +0530

    Update loc_netstat_op.py
    CSV File generation and Column Header Parsing
    First row will be column names

--- Integrated/loc_netstat_op.py
+++ Integrated/loc_netstat_op.py
@@ -1,5 +1,6 @@
 import platform
 import subprocess
+import csv
 print("All Listening Ports: ")
 operating_system = platform.system()
 command_win = ['netstat', '-ab']
@@ -17,20 +18,33 @@ text = p.stdout.read()
 # output fetched in the "Text" Variable as a string, later data can be extracted with regular expressions.
 # one = line by line
 one = list(text.decode().split("\n"))
-#
+output_in_list = []
+
+# Get Column Names
 two = one[3].split(" ")
 output_in_list = []
-for j in range(2, len(one)):
+three = []
+two = one[1]
+two = two.split("  ")
+temp = two[1:]
+two = two[0].split(" ")
+two.extend(temp)
+
+for j in range(1, len(one)):
     three = []
     two = one[j].split(" ")
     for i in range(0, len(two)):
         if two[i] != '':
             three.append(two[i])
     output_in_list.append(three)
+    
 del one
 del two
 del three
 del text
+with open("main_output.csv", "w") as f:
+    writer = csv.writer(f)
+    writer.writerows(output_in_list)
 
 print(output_in_list)
 p.kill()
