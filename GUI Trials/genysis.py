4bc469f1c7e8783958a30aff6cdef8417f32cb47
TejasDevda <Tejasjain803@gmail.com>
2019-02-25 14:58:03 +0530

    updated GUI

--- GUI Trials/genysis.py
+++ GUI Trials/genysis.py
@@ -28,10 +28,10 @@ class Ui_TabWidget(object):
         self.tab.setAutoFillBackground(True)
         self.tab.setObjectName("tab")
         self.tableWidget = QtWidgets.QTableWidget(self.tab)
-        self.tableWidget.setGeometry(QtCore.QRect(0, 0, 1290, 670))
+        self.tableWidget.setGeometry(QtCore.QRect(0, 0, 1295, 630))
         self.tableWidget.setAutoFillBackground(True)
-        self.tableWidget.setFrameShape(QtWidgets.QFrame.NoFrame)
-        self.tableWidget.setLineWidth(0)
+        self.tableWidget.setFrameShape(QtWidgets.QFrame.Box)
+        self.tableWidget.setLineWidth(1)
         self.tableWidget.setVerticalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOn)
         self.tableWidget.setHorizontalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOn)
         self.tableWidget.setSizeAdjustPolicy(QtWidgets.QAbstractScrollArea.AdjustToContents)
@@ -42,11 +42,11 @@ class Ui_TabWidget(object):
         self.tableWidget.setTextElideMode(QtCore.Qt.ElideLeft)
         self.tableWidget.setVerticalScrollMode(QtWidgets.QAbstractItemView.ScrollPerItem)
         self.tableWidget.setWordWrap(False)
-        self.tableWidget.setRowCount(30)
+        self.tableWidget.setRowCount(10)
         self.tableWidget.setColumnCount(6)
         self.tableWidget.setObjectName("tableWidget")
         item = QtWidgets.QTableWidgetItem()
-        item.setTextAlignment(QtCore.Qt.AlignJustify|QtCore.Qt.AlignVCenter)
+        item.setTextAlignment(QtCore.Qt.AlignLeading|QtCore.Qt.AlignVCenter)
         self.tableWidget.setHorizontalHeaderItem(0, item)
         item = QtWidgets.QTableWidgetItem()
         item.setTextAlignment(QtCore.Qt.AlignJustify|QtCore.Qt.AlignVCenter)
@@ -54,6 +54,7 @@ class Ui_TabWidget(object):
         item = QtWidgets.QTableWidgetItem()
         self.tableWidget.setHorizontalHeaderItem(2, item)
         item = QtWidgets.QTableWidgetItem()
+        item.setTextAlignment(QtCore.Qt.AlignLeading|QtCore.Qt.AlignVCenter)
         self.tableWidget.setHorizontalHeaderItem(3, item)
         item = QtWidgets.QTableWidgetItem()
         self.tableWidget.setHorizontalHeaderItem(4, item)
@@ -63,6 +64,9 @@ class Ui_TabWidget(object):
         self.tableWidget.horizontalHeader().setDefaultSectionSize(190)
         self.tableWidget.horizontalHeader().setMinimumSectionSize(120)
         self.tableWidget.horizontalHeader().setStretchLastSection(True)
+        self.pushButton = QtWidgets.QPushButton(self.tab)
+        self.pushButton.setGeometry(QtCore.QRect(600, 640, 89, 25))
+        self.pushButton.setObjectName("pushButton")
         TabWidget.addTab(self.tab, "")
         self.tab1 = QtWidgets.QWidget()
         sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
@@ -73,6 +77,11 @@ class Ui_TabWidget(object):
         self.tab1.setFocusPolicy(QtCore.Qt.WheelFocus)
         self.tab1.setAutoFillBackground(True)
         self.tab1.setObjectName("tab1")
+        self.frame = QtWidgets.QFrame(self.tab1)
+        self.frame.setGeometry(QtCore.QRect(0, 0, 1290, 670))
+        self.frame.setFrameShape(QtWidgets.QFrame.StyledPanel)
+        self.frame.setFrameShadow(QtWidgets.QFrame.Raised)
+        self.frame.setObjectName("frame")
         TabWidget.addTab(self.tab1, "")
         self.tab_3 = QtWidgets.QWidget()
         sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
@@ -118,6 +127,7 @@ class Ui_TabWidget(object):
         item.setText(_translate("TabWidget", "Country"))
         item = self.tableWidget.horizontalHeaderItem(5)
         item.setText(_translate("TabWidget", "Status"))
+        self.pushButton.setText(_translate("TabWidget", "LOAD"))
         TabWidget.setTabText(TabWidget.indexOf(self.tab), _translate("TabWidget", "Connections"))
         TabWidget.setTabText(TabWidget.indexOf(self.tab1), _translate("TabWidget", "Graphical View"))
         self.textBrowser.setHtml(_translate("TabWidget", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
@@ -126,13 +136,15 @@ class Ui_TabWidget(object):
 "</style></head><body style=\" font-family:\'Ubuntu\'; font-size:11pt; font-weight:400; font-style:normal;\">\n"
 "<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><br /></p>\n"
 "<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-size:16pt; font-weight:600; color:#5c3566;\"><br /></p>\n"
-"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:16pt; font-weight:600; color:#5c3566;\">Genysis Network Analysis Tool</span></p>\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:24pt; font-weight:600; color:#5c3566;\">Genysis Network Analysis Tool</span></p>\n"
 "<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-size:16pt; font-weight:600; text-decoration: underline;\"><br /></p>\n"
-"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">This tool is made is made by Team Lambda .</p>\n"
-"<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><br /></p>\n"
-"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">This tool helps in analysis of the onging processes going in the background and helps the user to  identify the type of access an process is trying to get without the information of user. This tool tell  who is trying to access to the computer and identifies the rootkits that maliciously works on you computer.</p>\n"
-"<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><br /></p>\n"
-"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">The graphical view also gives a clear vision of the network connections.  </p></body></html>"))
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:14pt;\">This tool is made is made by Team Lambda .</span></p>\n"
+"<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-size:14pt;\"><br /></p>\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:14pt;\">This tool helps in analysis of the onging processes going in the background and helps the user to  identify the type of access an process is trying to get without the information of user. This tool tell  who is trying to access to the computer and identifies the rootkits that maliciously works on you computer.</span></p>\n"
+"<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-size:14pt;\"><br /></p>\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:14pt;\">The graphical view also gives a clear vision of the network connections.  </span></p>\n"
+"<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-size:14pt;\"><br /></p>\n"
+"<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-size:14pt;\"><br /></p></body></html>"))
         TabWidget.setTabText(TabWidget.indexOf(self.tab_3), _translate("TabWidget", "About"))
 
 
