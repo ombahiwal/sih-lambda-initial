5a94a698fb0f7f65966b89d302b5b2c66a24c864
TejasDevda <Tejasjain803@gmail.com>
2019-02-25 17:14:12 +0530

    dynamic screen trial

---
+++ testing-for-anup/dynamic-screen.py
@@ -0,0 +1,175 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'genysis.ui'
+#
+# Created by: PyQt5 UI code generator 5.12
+#
+# WARNING! All changes made in this file will be lost!
+
+import sys
+from PyQt5 import QtCore, QtGui, QtWidgets
+"""
+app = QtWidgets.QApplication(sys.argv)
+screen = app.primaryScreen()
+size = screen.size()
+rect = screen.availableGeometry()
+length= rect.width()
+breadth= rect.height()
+
+"""
+class Ui_TabWidget(object):
+    def setupUi(self, TabWidget):
+  #      length= 1299
+   #     breadth= 741
+        TabWidget.setObjectName("TabWidget")
+        TabWidget.resize(length,breadth)
+        TabWidget.setFocusPolicy(QtCore.Qt.WheelFocus)
+        TabWidget.setWindowOpacity(1.0)
+        TabWidget.setAutoFillBackground(True)
+        TabWidget.setTabPosition(QtWidgets.QTabWidget.North)
+        TabWidget.setTabShape(QtWidgets.QTabWidget.Rounded)
+        TabWidget.setMovable(True)
+        self.tab = QtWidgets.QWidget()
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.tab.sizePolicy().hasHeightForWidth())
+        self.tab.setSizePolicy(sizePolicy)
+        self.tab.setAutoFillBackground(True)
+        self.tab.setObjectName("tab")
+        self.tableWidget = QtWidgets.QTableWidget(self.tab)
+        self.tableWidget.setGeometry(QtCore.QRect(0, 0,length,breadth))
+        self.tableWidget.setAutoFillBackground(True)
+        self.tableWidget.setFrameShape(QtWidgets.QFrame.Box)
+        self.tableWidget.setLineWidth(1)
+        self.tableWidget.setVerticalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOn)
+        self.tableWidget.setHorizontalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOn)
+        self.tableWidget.setSizeAdjustPolicy(QtWidgets.QAbstractScrollArea.AdjustToContents)
+        self.tableWidget.setAutoScrollMargin(10)
+        self.tableWidget.setDragEnabled(True)
+        self.tableWidget.setDragDropOverwriteMode(False)
+        self.tableWidget.setAlternatingRowColors(True)
+        self.tableWidget.setTextElideMode(QtCore.Qt.ElideLeft)
+        self.tableWidget.setVerticalScrollMode(QtWidgets.QAbstractItemView.ScrollPerItem)
+        self.tableWidget.setWordWrap(False)
+        self.tableWidget.setRowCount(10)
+        self.tableWidget.setColumnCount(6)
+        self.tableWidget.setObjectName("tableWidget")
+        item = QtWidgets.QTableWidgetItem()
+        item.setTextAlignment(QtCore.Qt.AlignLeading|QtCore.Qt.AlignVCenter)
+        self.tableWidget.setHorizontalHeaderItem(0, item)
+        item = QtWidgets.QTableWidgetItem()
+        item.setTextAlignment(QtCore.Qt.AlignJustify|QtCore.Qt.AlignVCenter)
+        self.tableWidget.setHorizontalHeaderItem(1, item)
+        item = QtWidgets.QTableWidgetItem()
+        self.tableWidget.setHorizontalHeaderItem(2, item)
+        item = QtWidgets.QTableWidgetItem()
+        item.setTextAlignment(QtCore.Qt.AlignLeading|QtCore.Qt.AlignVCenter)
+        self.tableWidget.setHorizontalHeaderItem(3, item)
+        item = QtWidgets.QTableWidgetItem()
+        self.tableWidget.setHorizontalHeaderItem(4, item)
+        item = QtWidgets.QTableWidgetItem()
+        self.tableWidget.setHorizontalHeaderItem(5, item)
+        self.tableWidget.horizontalHeader().setCascadingSectionResizes(False)
+        self.tableWidget.horizontalHeader().setDefaultSectionSize(190)
+        self.tableWidget.horizontalHeader().setMinimumSectionSize(120)
+        self.tableWidget.horizontalHeader().setStretchLastSection(True)
+        self.pushButton = QtWidgets.QPushButton(self.tab)
+        self.pushButton.setGeometry(QtCore.QRect(600, 640, 89, 25))
+        self.pushButton.setObjectName("pushButton")
+        TabWidget.addTab(self.tab, "")
+        self.tab1 = QtWidgets.QWidget()
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.tab1.sizePolicy().hasHeightForWidth())
+        self.tab1.setSizePolicy(sizePolicy)
+        self.tab1.setFocusPolicy(QtCore.Qt.WheelFocus)
+        self.tab1.setAutoFillBackground(True)
+        self.tab1.setObjectName("tab1")
+        self.frame = QtWidgets.QFrame(self.tab1)
+        self.frame.setGeometry(QtCore.QRect(0, 0, 1290, 670))
+        self.frame.setFrameShape(QtWidgets.QFrame.StyledPanel)
+        self.frame.setFrameShadow(QtWidgets.QFrame.Raised)
+        self.frame.setObjectName("frame")
+        TabWidget.addTab(self.tab1, "")
+        self.tab_3 = QtWidgets.QWidget()
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.tab_3.sizePolicy().hasHeightForWidth())
+        self.tab_3.setSizePolicy(sizePolicy)
+        self.tab_3.setFocusPolicy(QtCore.Qt.NoFocus)
+        self.tab_3.setAutoFillBackground(True)
+        self.tab_3.setObjectName("tab_3")
+        self.textBrowser = QtWidgets.QTextBrowser(self.tab_3)
+        self.textBrowser.setGeometry(QtCore.QRect(0, 0, 1290, 670))
+        self.textBrowser.setFrameShape(QtWidgets.QFrame.NoFrame)
+        self.textBrowser.setFrameShadow(QtWidgets.QFrame.Plain)
+        self.textBrowser.setLineWidth(0)
+        self.textBrowser.setVerticalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOff)
+        self.textBrowser.setHorizontalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOff)
+        self.textBrowser.setSizeAdjustPolicy(QtWidgets.QAbstractScrollArea.AdjustToContents)
+        self.textBrowser.setTabChangesFocus(False)
+        self.textBrowser.setDocumentTitle("")
+        self.textBrowser.setUndoRedoEnabled(False)
+        self.textBrowser.setLineWrapMode(QtWidgets.QTextEdit.WidgetWidth)
+        self.textBrowser.setObjectName("textBrowser")
+        TabWidget.addTab(self.tab_3, "")
+
+        self.retranslateUi(TabWidget)
+        TabWidget.setCurrentIndex(0)
+        QtCore.QMetaObject.connectSlotsByName(TabWidget)
+
+    def retranslateUi(self, TabWidget):
+        _translate = QtCore.QCoreApplication.translate
+        TabWidget.setWindowTitle(_translate("TabWidget", "Genysis Network Analysis Tool"))
+        self.tableWidget.setSortingEnabled(True)
+        item = self.tableWidget.horizontalHeaderItem(0)
+        item.setText(_translate("TabWidget", "Process ID"))
+        item = self.tableWidget.horizontalHeaderItem(1)
+        item.setText(_translate("TabWidget", "Application Name"))
+        item = self.tableWidget.horizontalHeaderItem(2)
+        item.setText(_translate("TabWidget", "Foriegn IP Address"))
+        item = self.tableWidget.horizontalHeaderItem(3)
+        item.setText(_translate("TabWidget", "Organisation Name"))
+        item = self.tableWidget.horizontalHeaderItem(4)
+        item.setText(_translate("TabWidget", "Country"))
+        item = self.tableWidget.horizontalHeaderItem(5)
+        item.setText(_translate("TabWidget", "Status"))
+        self.pushButton.setText(_translate("TabWidget", "LOAD"))
+        TabWidget.setTabText(TabWidget.indexOf(self.tab), _translate("TabWidget", "Connections"))
+        TabWidget.setTabText(TabWidget.indexOf(self.tab1), _translate("TabWidget", "Graphical View"))
+        self.textBrowser.setHtml(_translate("TabWidget", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+"<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
+"p, li { white-space: pre-wrap; }\n"
+"</style></head><body style=\" font-family:\'Ubuntu\'; font-size:11pt; font-weight:400; font-style:normal;\">\n"
+"<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><br /></p>\n"
+"<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-size:16pt; font-weight:600; color:#5c3566;\"><br /></p>\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:24pt; font-weight:600; color:#5c3566;\">Genysis Network Analysis Tool</span></p>\n"
+"<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-size:16pt; font-weight:600; text-decoration: underline;\"><br /></p>\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:14pt;\">This tool is made is made by Team Lambda .</span></p>\n"
+"<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-size:14pt;\"><br /></p>\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:14pt;\">This tool helps in analysis of the onging processes going in the background and helps the user to  identify the type of access an process is trying to get without the information of user. This tool tell  who is trying to access to the computer and identifies the rootkits that maliciously works on you computer.</span></p>\n"
+"<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-size:14pt;\"><br /></p>\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:14pt;\">The graphical view also gives a clear vision of the network connections.  </span></p>\n"
+"<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-size:14pt;\"><br /></p>\n"
+"<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-size:14pt;\"><br /></p></body></html>"))
+        TabWidget.setTabText(TabWidget.indexOf(self.tab_3), _translate("TabWidget", "About"))
+
+
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    screen = app.primaryScreen()
+    size = screen.size()
+    rect = screen.availableGeometry()
+    length = rect.width()
+    breadth = rect.height()
+    TabWidget = QtWidgets.QTabWidget()
+    ui = Ui_TabWidget()
+    ui.setupUi(TabWidget)
+    TabWidget.show()
+    sys.exit(app.exec_())
